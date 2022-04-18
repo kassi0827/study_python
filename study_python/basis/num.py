c8ae2290677550764d17e0b72ac4f2fd72f209c3
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2022-04-18 22:25:12 +0900

    3-10 数値

---
+++ study_python/basis/num.py
@@ -0,0 +1,70 @@
+import math
+
+result = math.sqrt(25)
+print(result)
+
+y = math.log2(10)
+print(y)
+
+# 以下のコードでライブラリのドキュメントを読むことができる
+# print(help(math))
+
+# kashimataichi$ python
+# WARNING: Python 2.7 is not recommended.
+# This version is included in macOS for compatibility with legacy software.
+# Future versions of macOS will not include Python 2.7.
+# Instead, it is recommended that you transition to using 'python3' from within Terminal.
+
+# Python 2.7.16 (default, Mar 25 2021, 03: 11: 28)
+# [GCC 4.2.1 Compatible Apple LLVM 11.0.3 (clang-1103.0.29.20)(-macos10.15-objc - on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >> > 2 + 2
+# 4
+# >> > 5 - 2
+# 3
+# >> > 5 * 6
+# 30
+# >> > 50 - 5 * 6
+# 20
+# >> > (50-5)*6
+# 270
+# >> > 8/5
+# 1
+# >> > type(1)
+# < type 'int' >
+# >> > type(1.6)
+# < type 'float' >
+# >> > 0.6
+# 0.6
+# >> > .6
+# 0.6
+# >> > 17/3
+# 5
+# >> > 17//3
+# 5
+# >> > 17 % 3
+# 2
+# >> > 5*5
+# 25
+# >> > 5 ** 2
+# 25
+# >> > 5*5*5*5*5
+# 3125
+# >> > 5 ** 5
+# 3125
+# >> > x=5
+# >> > x
+# 5
+# >> > y=10
+# >> > x * y
+# 50
+# >> > a
+# Traceback(most recent call last):
+# File "<stdin>", line 1, in <module >
+# NameError: name 'a' is not defined
+# >> > pie=3.141594374384774
+# >> > pie
+# 3.141594374384774
+# >> > round(pie, 2)
+# 3.14
+# >> > exit()
