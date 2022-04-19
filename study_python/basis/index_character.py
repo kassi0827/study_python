ed79e862186a3d539bb747d1071246c281262b17
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2022-04-19 22:13:59 +0900

    12. 文字列のインデックスとスライス

---
+++ study_python/basis/index_character.py
@@ -0,0 +1,17 @@
+word = 'python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print(word[0:2])
+print(word[2:5])
+print('###################')
+print(word[0:2])
+print(word[:2])
+print('###################')
+print(word[2:])
+print('###################')
+word = 'j' + word[1:]
+print(word[:])
+
+n = len(word)
+print(n)
