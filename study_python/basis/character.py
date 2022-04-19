e9a016b0cbbefa0759dab8c052e985f0e8479b95
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2022-04-19 21:58:28 +0900

    11. 文字列

---
+++ study_python/basis/character.py
@@ -0,0 +1,24 @@
+print('hello')
+print("hello")
+print("I don't know")
+print('I don\'t know')
+print('say "I don\'t know"')
+print("say \"I don't know\"")
+
+print('hello.\nHow are you?')
+print(r'C:\name\name')
+
+print("###############")
+print("""\
+line1
+line2
+line3\
+""")
+print("###############")
+
+print('Hi.' * 3 + 'Mike.')
+
+s = ('aaaaaaaaaaaaaaaaaaaaaaaaaa'
+     'bbbbbbbbbbbbbbbbbbbbbbbbbb')
+
+print(s)
