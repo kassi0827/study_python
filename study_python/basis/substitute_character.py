0bc1d6a76c664fd8d496e1e4813f98eb96af79cd
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2022-04-20 22:03:26 +0900

    14. 文字の代入

---
+++ study_python/basis/substitute_character.py
@@ -0,0 +1,39 @@
+# DarkDream: pg kashimataichi$ python
+
+# WARNING: Python 2.7 is not recommended.
+# This version is included in macOS for compatibility with legacy software.
+# Future versions of macOS will not include Python 2.7.
+# Instead, it is recommended that you transition to using 'python3' from within Terminal.
+
+# Python 2.7.16 (default, Mar 25 2021, 03: 11: 28)
+# [GCC 4.2.1 Compatible Apple LLVM 11.0.3 (clang-1103.0.29.20)(-macos10.15-objc - on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >> > 'a is {}'.format('a')
+# 'a is a'
+# >> > 'a is {}'.format('test')
+# 'a is test'
+# >> > 'a is {} {} {}'.format(1, 2, 3)
+# 'a is 1 2 3'
+# >> > 'a is {0} {1} {2}'.format(1, 2, 3)
+# 'a is 1 2 3'
+# >> > 'a is {2} {1} {0}'.format(1, 2, 3)
+# 'a is 3 2 1'
+# >> > 'My name is {0} {1}'.format('Jun', 'Sakai')
+# 'My name is Jun Sakai'
+# >> > 'My name is {0} {1}. Watashi ha {1} {0}'.format('Jun', 'Sakai')
+# 'My name is Jun Sakai. Watashi ha Sakai Jun'
+# >> > 'My name is {name} {family}. Watashi ha {family} {name}'.format(name='Jun', family='Sakai')
+# 'My name is Jun Sakai. Watashi ha Sakai Jun'
+# >> > 1
+# 1
+# >> > '1'
+# '1'
+# >> > str(1)
+# '1'
+# >> > x=str(1)
+# >> > type(x)
+# < type 'str' >
+# >> > str(3.14)
+# '3.14'
+# >> > str(True)
+# 'True'
