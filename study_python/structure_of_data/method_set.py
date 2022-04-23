a289f36b84db989363252496c4a023e2a9cefe15
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2022-04-23 21:15:58 +0900

    29.集合のメソッド

---
+++ study_python/structure_of_data/method_set.py
@@ -0,0 +1,28 @@
+# Python 2.7.16 (default, Mar 25 2021, 03: 11: 28)
+# [GCC 4.2.1 Compatible Apple LLVM 11.0.3 (clang-1103.0.29.20)(-macos10.15-objc - on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >> > s={1, 2, 3, 4, 5}
+# >> > s[0]
+# Traceback(most recent call last):
+# File "<stdin>", line 1, in <module >
+# TypeError: 'set' object does not support indexing
+# >> > s.add(6)
+# >> > s
+# set([
+# 1, 2, 3, 4, 5, 6])
+# >> > s.add(6)
+# >> > s
+# set([
+# 1, 2, 3, 4, 5, 6])
+# >> > s.remove(6)
+# >> > s
+# set([1, 2, 3, 4, 5])
+# >> > s.clear()
+# >> > s
+# set([])
+# >> > a={}
+# >> > type(a)
+# < type 'dict' >
+# >> > a
+# {}
+# >> >
